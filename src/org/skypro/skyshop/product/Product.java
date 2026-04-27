fa35a232ca4702926f4f5265ea4c63f7b503ff1f
Dmitriy <d.sherin@list.ru>
2026-04-27 19:32:02 +0700

    Создание класса товара

---
+++ src/org/skypro/skyshop/product/Product.java
@@ -0,0 +1,14 @@
+package org.skypro.skyshop.product;
+
+public class Product {
+    private String Name;
+    private int price;
+
+    public String getName() {
+        return Name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
