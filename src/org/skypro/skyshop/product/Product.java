7a5e835481058cba72bcb9372cbdb0397768ec25
Dmitriy <d.sherin@list.ru>
2026-05-07 21:54:10 +0700

    Сделал проверку в классе Product

--- src/org/skypro/skyshop/product/Product.java
+++ src/org/skypro/skyshop/product/Product.java
@@ -4,8 +4,12 @@ import org.skypro.skyshop.search.Searchable;
 public abstract class Product implements Searchable {
     private final String name;
 
-    public Product(String name) {
-        this.name = name;
+    public Product(String _name) {
+        if (_name == null || _name.isBlank()) {
+            throw new IllegalArgumentException("Название продукта не может быть пустым или состоять только из пробелов");
+        }
+
+        this.name = Name;
     }
     @Override
     public String getSearchTerm() {
